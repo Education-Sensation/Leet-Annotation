4744a7ca67a38168727fc00cbfe546fbefd5c7f5
Mark <themarkymarkers@gmail.com>
2020-12-17 19:00:40 -0800

    draft static AddNoteForm component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import AddNoteForm from "./addNoteForm";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>Hello World</h1>
       <h1>Hello World</h1>
       <h1>Github pages is so broken</h1>
+      <AddNoteForm />
     </div>
   );
 }
