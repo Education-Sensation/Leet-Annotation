444dd54705623d16ea519ec8c61fc6fde690acd2
Mark <themarkymarkers@gmail.com>
2020-12-19 13:47:19 -0800

    refactor App as a class component

--- src/App.js
+++ src/App.js
@@ -5,35 +5,39 @@ import AddNoteForm from "./addNoteForm";
 import AddReadingTextForm from "./addReadingTextForm";
 import NoteDisplayUI from "./noteDisplayUI";
 
-const App = () => {
-  const [readingText, setReadingText] = useState([]);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {readingText: this.getText()};  // readingText is an array of strings each representing paragraphs
+ }
 
-  useEffect(() => {
-    getText();
-  }, []);
+  setReadingText(newText) {
+    this.setState({readingText: newText});
+  }
 
-  const getText = async () => {
+  getText = async () => {
     const response = await fetch(
       "https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1"
     );
-    const data = await response.json();
-    setReadingText(data);
+    const data = await response.json();  // TODO: please turn the data varaible into an array. render() expects this.state.readingText to be an array
+    this.setReadingText(data);
   };
 
-  const firstRendered = readingText.map((item, index) => {
-    return <div key={index}>{item}</div>;
-  });
-
-  console.log(readingText);
-
-  return (
-    <div className="App">
-      <div className="readingText">{firstRendered}</div>
-      <NoteDisplayUI />
-      <AddReadingTextForm readingText={readingText} onSubmit={setReadingText} />
-      <AddNoteForm />
-    </div>
-  );
+  render() {
+    const paragraphs = ['graph1', 'graph2', 'graph3'].map((item) => <p>{item}</p>);
+    // const paragraphs = this.state.readingText.map((item, index) => {
+    //   return <p key={index}>{item}</p>;
+    // });
+    
+    return (
+      <div className="App">
+        <div className="readingText">{paragraphs}</div>
+        <NoteDisplayUI />
+        <AddReadingTextForm onSubmit={this.setReadingText} />
+        <AddNoteForm />
+      </div>
+    );  
+  }
 };
 
 export default App;
