dc6edff09754f9277d5827462156998c32518305
Mark <themarkymarkers@gmail.com>
2020-12-18 18:09:11 -0800

    implement AddNoteForm as class

--- src/addNoteForm.js
+++ src/addNoteForm.js
@@ -1,33 +1,39 @@
 import React from "react";
 import AddTagForm from "./addTagForm";
 
-export default function AddNoteForm() {
-  return (
-    <div>
-      <h3>New Note</h3>
-      <form>
-        <label for="note-field"></label>
-        <textarea
-          name="note-field"
-          id="note-field"
-          placeholder="have any thoughts to write?"
-        ></textarea>
+export default class AddNoteForm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-        <label for="keyphrase-field">key phrase</label>
-        <input name="keyphrase-field" id="keyphrase-field" type="text"></input>
+    render() {
+      return (
+        <div>
+            <h3>New Note</h3>
+            <form>
+            <label for="note-field"></label>
+            <textarea
+                name="note-field"
+                id="note-field"
+                placeholder="have any thoughts to write?"
+            ></textarea>
 
-        <label for="tags-field">tags</label>
-        <input name="tags-field" id="tags-field" type="text"></input>
+            <label for="keyphrase-field">key phrase</label>
+            <input name="keyphrase-field" id="keyphrase-field" type="text"></input>
 
-        {/* shows <AddTagForm /> */}
-        <button>create new tag &#12297;</button>
+            <label for="tags-field">tags</label>
+            <input name="tags-field" id="tags-field" type="text"></input>
 
-        {/* add this note to the App component’s dictionary */}
-        <button>Enter</button>
+            {/* shows <AddTagForm /> */}
+            <button>create new tag &#12297;</button>
 
-        <AddTagForm />
+            {/* add this note to the App component’s dictionary */}
+            <button>Enter</button>
 
-      </form>
-    </div>
-  );
-}
+            <AddTagForm />
+
+            </form>
+        </div>
+      );
+    }
+  }
