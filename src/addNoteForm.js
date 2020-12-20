7cfa95e37723a01c6078e80be36185b02ed14779
gelafin <46856925+gelafin@users.noreply.github.com>
2020-12-20 13:53:18 -0800

    Add CSS styling
    * Added bootstrap components, Jumbotron, Accordian
    
    Added bootstrap components, Jumbotron, Accordian
    
    * Added sticky footer, navbar and have started to add buttons to UI
    
    * Added small bit of logic to new reading text, wont accept an empty field, continued styling components
    
    * Added new button styling, messing around with navbar and jumbotron colors
    
    * Added button styling to mutliple components
    
    * Added gradient css style to jumbotron
    
    * Added another button
    
    * Changed button, and width and justify to text content
    
    * Fixed merge conficts and added import line
    
    Co-authored-by: iamkevin <bowerke@oregonstate.edu>

--- src/addNoteForm.js
+++ src/addNoteForm.js
@@ -1,120 +1,168 @@
 import React from "react";
 import AddTagForm from "./addTagForm";
 import TagSelector from "./tagSelector";
+import Button from "react-bootstrap/Button";
 
 // Return an array of the selected opion values
 // select is an HTML select element
 function getSelectValues(selectElement) {
-    var result = [];
-    var options = selectElement && selectElement.options;
-    var option;
-  
-    for (var i=0, iLen=options.length; i<iLen; i++) {
-      option = options[i];
-  
-      if (option.selected) {
-        result.push(option.value || option.text);
-      }
+  var result = [];
+  var options = selectElement && selectElement.options;
+  var option;
+
+  for (var i = 0, iLen = options.length; i < iLen; i++) {
+    option = options[i];
+
+    if (option.selected) {
+      result.push(option.value || option.text);
     }
-    return result;
   }
+  return result;
+}
 
 export default class AddNoteForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tagFormIsHidden: true,
-            formData: {
-                note: '',
-                keyPhrase: '',
-                tags: [],
-            }
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      tagFormIsHidden: true,
+      formData: {
+        note: "",
+        keyPhrase: "",
+        tags: [],
+      },
+    };
 
-        this.handleTagFormDisplay = this.handleTagFormDisplay.bind(this);
-        this.handleNoteChange = this.handleNoteChange.bind(this);
-        this.handleKeyPhraseChange = this.handleKeyPhraseChange.bind(this);
-        this.handleTagsChange = this.handleTagsChange.bind(this);
-        this.submitNewNote = this.submitNewNote.bind(this);
-    }
+    this.handleTagFormDisplay = this.handleTagFormDisplay.bind(this);
+    this.handleNoteChange = this.handleNoteChange.bind(this);
+    this.handleKeyPhraseChange = this.handleKeyPhraseChange.bind(this);
+    this.handleTagsChange = this.handleTagsChange.bind(this);
+    this.submitNewNote = this.submitNewNote.bind(this);
+  }
 
-    handleTagFormDisplay(shouldHide) {
-        // if bool shouldHide is true, hides form; else, shows form        
-        this.setState({tagFormIsHidden: shouldHide});
-    }
+  handleTagFormDisplay(shouldHide) {
+    // if bool shouldHide is true, hides form; else, shows form
+    this.setState({ tagFormIsHidden: shouldHide });
+  }
 
-    handleNoteChange(event) {
-        this.setState({formData: {...this.state.formData, note: event.target.value}})
-    }
+  handleNoteChange(event) {
+    this.setState({
+      formData: { ...this.state.formData, note: event.target.value },
+    });
+  }
 
-    handleKeyPhraseChange(event) {
-        this.setState({formData: {...this.state.formData, keyPhrase: event.target.value}})
-    }
+  handleKeyPhraseChange(event) {
+    this.setState({
+      formData: { ...this.state.formData, keyPhrase: event.target.value },
+    });
+  }
 
-    handleTagsChange(event) {
-        const newTagArray = getSelectValues(event.target);
+  handleTagsChange(event) {
+    const newTagArray = getSelectValues(event.target);
 
-        this.setState((state, props) => {
-            console.log('replacing ', state.formData.tags, ' with ', newTagArray);
-            return {formData: {...state.formData, tags: newTagArray}};
-        });
-    }
+    this.setState((state, props) => {
+      console.log("replacing ", state.formData.tags, " with ", newTagArray);
+      return { formData: { ...state.formData, tags: newTagArray } };
+    });
+  }
 
-    submitNewNote() {
-        console.log('submitting this formData to App... ', this.state.formData);
-        this.props.submitNewNote(this.state.formData);
-        // TODO: is there an easy way to clear fields here?
-        alert('Note Added!');
-    }
+  submitNewNote() {
+    console.log("submitting this formData to App... ", this.state.formData);
+    this.props.submitNewNote(this.state.formData);
+    // TODO: is there an easy way to clear fields here?
+    alert("Note Added!");
+  }
 
-    render() {
-        // decide whether to show the <AddTagForm /> or just an empty div
-        const emptySpan = <span></span>;
-        let tagForm = emptySpan;
+  render() {
+    // decide whether to show the <AddTagForm /> or just an empty div
+    const emptySpan = <span></span>;
+    let tagForm = emptySpan;
 
-        if (!this.state.tagFormIsHidden) {
-            tagForm = <AddTagForm hideForm={this.handleTagFormDisplay} submitForm={this.props.submitNewTag} />;
-        }
-
-        return (
-            <div>
-                <h3>New Note</h3>
-                <form>
-                <label htmlFor="note-field"></label>
-                <textarea
-                    name="note-field"
-                    id="note-field"
-                    placeholder="have any thoughts to write?"
-                    onChange={this.handleNoteChange}
-                ></textarea>
-
-                <label htmlFor="keyphrase-field">key phrase</label>
-                <input name="keyphrase-field" id="keyphrase-field" type="text" onChange={this.handleKeyPhraseChange}></input>
-
-                {/* TODO: change this to use props */}
-                <label htmlFor="tags-field">tags</label>
-                <select multiple={true} name="tags-field" id="tags-field" onChange={this.handleTagsChange}>
-                    <option>tag1</option>
-                    <option>tag2</option>
-                    <option>tag3</option>
-                </select>
-
-                {/* testing before deleting the placeholder tags above */}
-                <TagSelector tagList={this.props.tagList} />
-
-                {/* shows <AddTagForm /> */}
-                <button type='button' onClick={() => {
-                        this.handleTagFormDisplay(false);
-                    }}>create new tag &#12297;</button>
-
-                {/* add this note to the App component’s dictionary */}
-                <button type='button' onClick={this.submitNewNote}>Enter</button>
-
-                </form>
-
-                {tagForm}
-
-            </div>
+    if (!this.state.tagFormIsHidden) {
+      tagForm = (
+        <AddTagForm
+          hideForm={this.handleTagFormDisplay}
+          submitForm={this.props.submitNewTag}
+        />
       );
     }
+
+    return (
+      <div>
+        <h4>Add a New Note</h4>
+        <form>
+          <label htmlFor="note-field"></label>
+
+          <textarea
+            className="newnote"
+            name="note-field"
+            id="note-field"
+            placeholder="have any thoughts to write?"
+            cols="30"
+            rows="5"
+            onChange={this.handleNoteChange}
+          ></textarea>
+          <br />
+
+          <label htmlFor="keyphrase-field">
+            <strong>Key Phrase:</strong>{" "}
+          </label>
+          <input
+            className="keyphrase"
+            name="keyphrase-field"
+            id="keyphrase-field"
+            type="text"
+            onChange={this.handleKeyPhraseChange}
+          ></input>
+
+          {/* TODO: change this to use props */}
+          <label htmlFor="tags-field">tags</label>
+          <select
+            multiple={true}
+            name="tags-field"
+            id="tags-field"
+            onChange={this.handleTagsChange}
+          >
+            <option>tag1</option>
+            <option>tag2</option>
+            <option>tag3</option>
+          </select>
+
+          {/* testing before deleting the placeholder tags above */}
+          <TagSelector tagList={this.props.tagList} />
+
+          <label htmlFor="tags-field">
+            <strong>Tags: </strong>{" "}
+          </label>
+          <input
+            className="tags"
+            name="tags-field"
+            id="tags-field"
+            type="text"
+          ></input>
+
+          {/* shows <AddTagForm /> */}
+          <Button
+            className="createNewTag"
+            variant="outline-secondary"
+            type="button"
+            onClick={() => {
+              this.handleTagFormDisplay(false);
+            }}
+          >
+            Create a new tag &#12297;
+          </Button>
+
+          {/* add this note to the App component’s dictionary */}
+          <Button
+            variant="outline-success"
+            onClick={this.submitNewNote}
+            type="button"
+          >
+            Enter
+          </Button>
+        </form>
+        {tagForm}
+      </div>
+    );
   }
+}
--- src/addReadingTextForm.js
+++ src/addReadingTextForm.js
@@ -1,39 +1,55 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 export default class addReadingTextForm extends React.Component {
   constructor(props) {
     super(props);
     this.handleAddClick = this.handleAddClick.bind(this);
     this.handleTextChange = this.handleTextChange.bind(this);
-    this.state = {newParagraphs: []};  // tracks textarea input
+    this.state = { newParagraphs: [] }; // tracks textarea input
   }
 
   handleAddClick() {
-    console.log('submitting this (hopefully new) reading text to App: ', this.state.newParagraphs);
-    this.props.submitNewReadingText(this.state.newParagraphs);  // pass textarea value (as recorded in state)
-  };
+    console.log(
+      "submitting this (hopefully new) reading text to App: ",
+      this.state.newParagraphs
+    );
+    if (this.state.newParagraphs != "") {
+      this.props.submitNewReadingText(this.state.newParagraphs); // pass textarea value (as recorded in state)
+    }
+  }
 
   handleTextChange(event) {
-    console.log('changing state in handleTextChange() from ', this.state.newParagraphs, ' to ', event.target.value);
-    this.setState({newParagraphs: event.target.value});
+    console.log(
+      "changing state in handleTextChange() from ",
+      this.state.newParagraphs,
+      " to ",
+      event.target.value
+    );
+    this.setState({ newParagraphs: event.target.value });
   }
 
   render() {
     return (
       <div className="newReadingStyle">
-        <h1>New Reading Text</h1>
+        <h3>Enter new text to be annotated:</h3>
         <form>
           <textarea
             name="newReadingText"
             cols="100"
             rows="10"
             onChange={this.handleTextChange}
-            //value={[]}
           ></textarea>
           {/* replaces text in ReadingText component */}
-          <button onClick={this.handleAddClick} className="newTextButton" type="button">
-            Add
-          </button>
+          <br />
+          <Button
+            variant="success"
+            onClick={this.handleAddClick}
+            className="newTextButton"
+            type="button"
+          >
+            Add!
+          </Button>
         </form>
       </div>
     );
